7e7cf871d2e1186b5072b95832bc78fdb5c66dca
Joydee Ghosh <joydeep.ghosh@cuc.org>
2018-05-24 10:43:58 -0500

    Created a solution for ACT Weekly Training Email

---
+++ ACTWeeklyRoundUp/Solution/SogetiACTTraining/Controllers/WeeklyRoundUpEmailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SogetiACTTraining.Controllers
+{
+    public class WeeklyRoundUpEmailController : Controller
+    {
+        // GET: WeeklyRoundUpEmail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
